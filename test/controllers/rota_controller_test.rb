821aad9a6684b413050a606ceecbf4dada4e016d
ynjisng <abreusara012@gmail.com>
2024-02-03 23:32:19 -0300

    corrige parametros de teste

--- test/controllers/rota_controller_test.rb
+++ test/controllers/rota_controller_test.rb
@@ -17,7 +17,7 @@ class RotaControllerTest < ActionDispatch::IntegrationTest
 
   test "should create rotum" do
     assert_difference("Rotum.count") do
-      post rota_url, params: { rotum: { caminhao_id: @rotum.caminhao_id, data_de_chegada: @rotum.data_de_chegada, data_de_partida: @rotum.data_de_partida, observacoes: @rotum.observacoes, endereco_chegada: @rotum.endereco_chegada_id, endereco_partida: @rotum.endereco_partida_id } }
+      post rota_url, params: { rotum: { caminhao_id: @rotum.caminhao_id, data_de_chegada: @rotum.data_de_chegada, data_de_partida: @rotum.data_de_partida, observacoes: @rotum.observacoes, endereco_chegada_id: @rotum.endereco_chegada_id, endereco_partida_id: @rotum.endereco_partida_id } }
     end
 
     assert_redirected_to rotum_url(Rotum.last)
