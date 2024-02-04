1795a0162a40bb1e2037529829b210173a4a15a9
ynjisng <abreusara012@gmail.com>
2024-02-03 21:45:34 -0300

    criação das validações dos models de endereço e rota

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,4 +1,7 @@
 class Endereco < ApplicationRecord
   # belongs_to :rotum
-  has_many :paradas, dependent: :destroy
+  #has_many :paradas, dependent: :destroy
+  has_many :rotum, dependent: :nullify
+  validates :cidade, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :cep, presence: true, format: { with: /\A\d{8}\z/, message: "deve conter 8 dígitos numéricos" }
 end
