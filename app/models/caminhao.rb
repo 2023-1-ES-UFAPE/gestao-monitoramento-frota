304e48b9e7ccfda6fa6264af811e0b0e057574b7
Lucas Emanoel <lucas2014.barros@gmail.com>
2024-02-07 01:05:43 -0300

    FEAT: adding validates - caminhao model

--- app/models/caminhao.rb
+++ app/models/caminhao.rb
@@ -1,4 +1,14 @@
 class Caminhao < ApplicationRecord
   has_one :rotum, dependent: :destroy
   has_many :paradas, dependent: :destroy
+
+  validates :modelo, presence: true, length: {minimum:32}, numericality: false
+  validates :placa, presence: true, uniqueness: true, length: {minimum:12}, numericality: false
+  validates :ano, presence: true, length: {minimum:9999}, numericality: true
+  validates :capacidade, presence: true, length: {minimum:1000}, numericality: false
+  validates :status, presence: true, length: {maximum:16}, numericality: false
+  validates :chassi, presence: true, uniqueness: true, length: {minimum:10}, numericality: false
+  validates :data_ultima_inspecao, on_or_before: lambda { Date.current }
+
+
 end
