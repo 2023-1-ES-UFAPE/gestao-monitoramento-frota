3f695252a446d8a4825b6b4ce91477c71cbe27bd
Lucas Emanoel <lucas2014.barros@gmail.com>
2024-02-07 13:26:55 -0300

    FIX: ajust validates - caminhao model

--- app/models/caminhao.rb
+++ app/models/caminhao.rb
@@ -2,13 +2,12 @@ class Caminhao < ApplicationRecord
   has_one :rotum, dependent: :destroy
   has_many :paradas, dependent: :destroy
 
-  validates :modelo, presence: true, length: {minimum:32}, numericality: false
-  validates :placa, presence: true, uniqueness: true, length: {minimum:12}, numericality: false
-  validates :ano, presence: true, length: {minimum:9999}, numericality: true
-  validates :capacidade, presence: true, length: {minimum:1000}, numericality: false
+  validates :modelo, presence: true, length: {minimum:8}, numericality: false
+  validates :placa, presence: true, length: {maximum:12}, numericality: false
+  validates :ano, presence: true, length: {maximum:9999}, numericality: true
+  validates :capacidade, presence: true, length: {maximum:1000}, numericality: false
   validates :status, presence: true, length: {maximum:16}, numericality: false
   validates :chassi, presence: true, uniqueness: true, length: {minimum:10}, numericality: false
-  validates :data_ultima_inspecao, on_or_before: lambda { Date.current }
 
 
 end
