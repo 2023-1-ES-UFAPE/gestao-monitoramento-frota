7b9147290b3630c676f92dea925cd6173015a73d
Sara Abreu <50270954+ynjisng@users.noreply.github.com>
2024-02-07 20:36:01 -0300

    testes unitários

--- test/models/endereco_test.rb
+++ test/models/endereco_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class EnderecoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "salvar endereco cidade invalido" do
+    endereco = Endereco.new(cidade: "abc", cep: 55293050)
+    assert_not(endereco.save)
+  end
+
+  test "salvar endereco cidade" do
+    endereco = Endereco.new(cidade: "abc", cep: 55293050)
+    assert(endereco.save)
+  end
 end
