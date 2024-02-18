406585d4e06b38f4c7b4c385e82968725bd4274d
lucas8790 <lv8790121@gmail.com>
2024-02-18 17:49:55 -0300

    Validações e relacionamentos para carga

--- app/models/carga.rb
+++ app/models/carga.rb
@@ -1,2 +1,10 @@
 class Carga < ApplicationRecord
+  belongs_to :caminhao
+  has_many :rotum, through: :caminhao
+
+  validates :descricao, presence: true
+  validates :peso, presence: true, numericality: { greater_than: 0 }
+  validates :volume, presence: true, numericality: { greater_than: 0 }
+  validates :valor, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :numero_rastreamento, presence: true, uniqueness: true
 end
