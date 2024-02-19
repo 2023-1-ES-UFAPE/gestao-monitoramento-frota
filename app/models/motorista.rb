6cd7e5124750b902fc8f70b4f532953515cf08a8
JuanGustah <juangt360graus@gmail.com>
2024-02-19 14:55:19 -0300

    feat: add model validations

--- app/models/motorista.rb
+++ app/models/motorista.rb
@@ -1,3 +1,16 @@
 class Motorista < ApplicationRecord
   belongs_to :caminhao
+
+  validates :nome, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :cpf, presence: true, length: { is: 11}, numericality: true
+  validates :cnh, presence: true, length: { is: 11}, numericality: true
+  validates :telefone, presence: true, length: { is: 11}, numericality: true
+  validate :valida_data_nascimento
+  validates :caminhao_id, presence: true
+
+  def valida_data_nascimento
+    if data_nascimento.blank? || data_nascimento > (Date.today - 18.years)
+      errors.add(:data_nascimento, ", Motorista deve ser maior de idade")
+    end
+  end
 end
