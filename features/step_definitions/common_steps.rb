793b67bc434a8c94af35df40c2f2c575dc623e08
Ricardo <contatorms7@tutamil.com>
2024-02-24 06:49:27 -0300

    Implementação da autenticação com Devise

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -18,3 +18,19 @@ end
 Then('I should not see {string}') do |text|
   expect(page).to_not have_content("#{text}")
 end
+
+# passos de autenticação antes dos testes do cucumber
+Given /^I am not authenticated$/ do
+  visit('/users/sign_out') # ensure that at least
+end
+
+Given /^I am a new, authenticated user$/ do
+  email = 'testing@man.net'
+  password = 'secretpass'
+  User.new(:email => email, :password => password, :password_confirmation => password).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Entrar"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,12 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+  # passo de autenticação antes dos testes de model
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @user = users(:one)
+    sign_in users(:one)
+  end
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
 
