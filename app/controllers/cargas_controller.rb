3900c0ae6a6150ee9efb94eea2445e457e95bcbe
ynjisng <abreusara012@gmail.com>
2024-02-25 14:19:17 -0300

    refactor: Refactoring - Extract Method carga_sucess

--- app/controllers/cargas_controller.rb
+++ app/controllers/cargas_controller.rb
@@ -25,8 +25,7 @@ class CargasController < ApplicationController
 
     respond_to do |format|
       if @carga_atual.save
-        format.html { redirect_to carga_path(@carga_atual), notice: "Carga was successfully created." }
-        format.json { render :show, status: :created, location: @carga_atual }
+        carga_success(format, :created, "created")
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @carga_atual.errors, status: :unprocessable_entity }
@@ -38,8 +37,7 @@ class CargasController < ApplicationController
   def update
     respond_to do |format|
       if @carga_atual.update(carga_params)
-        format.html { redirect_to carga_url(@carga_atual), notice: "Carga was successfully updated." }
-        format.json { render :show, status: :ok, location: @carga_atual }
+        carga_success(format, :ok, "updated")
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @carga_atual.errors, status: :unprocessable_entity }
@@ -58,13 +56,19 @@ class CargasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_carga
-      @carga_atual = Carga.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def carga_params
-      params.require(:carga).permit(:descricao, :peso, :volume, :valor, :numero_rastreamento, :rota_id)
-    end
+  def carga_success(format, status, method)
+    format.html { redirect_to carga_url(@carga_atual), notice: "Carga was successfully #{method}." }
+    format.json { render :show, status: status, location: @carga_atual }
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_carga
+    @carga_atual = Carga.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def carga_params
+    params.require(:carga).permit(:descricao, :peso, :volume, :valor, :numero_rastreamento, :rota_id)
+  end
 end
