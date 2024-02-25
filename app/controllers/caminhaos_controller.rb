2ac4be2da874499b6dbc9a482d3b4baeda2419b6
Lucas Victor <71571447+lucasvictoor@users.noreply.github.com>
2024-02-25 13:04:32 -0300

    Adição de 3 refatorações do tipo renomeação de variável (#58)
    * Refactoring - Rename Variable
    
    Renomeia a variável @caminhao para @caminhao_atual para melhorar a clareza e a semântica do código.
    
    * Refactoring - Rename Variable
    
    Renomeia a variável @carga para @carga_atual para melhorar a clareza e a semântica do código.
    
    * Refactoring - Rename Variable
    
    Renomeia a variável @endereco para @endereco_atual para melhorar a clareza e a semântica do código.

--- app/controllers/caminhaos_controller.rb
+++ app/controllers/caminhaos_controller.rb
@@ -12,7 +12,7 @@ class CaminhaosController < ApplicationController
 
   # GET /caminhaos/new
   def new
-    @caminhao = Caminhao.new
+    @caminhao_atual = Caminhao.new
   end
 
   # GET /caminhaos/1/edit
@@ -21,15 +21,15 @@ class CaminhaosController < ApplicationController
 
   # POST /caminhaos or /caminhaos.json
   def create
-    @caminhao = Caminhao.new(caminhao_params)
+    @caminhao_atual = Caminhao.new(caminhao_params)
 
     respond_to do |format|
-      if @caminhao.save
-        format.html { redirect_to caminhao_url(@caminhao), notice: "Caminhao was successfully created." }
-        format.json { render :show, status: :created, location: @caminhao }
+      if @caminhao_atual.save
+        format.html { redirect_to caminhao_url(@caminhao_atual), notice: "Caminhao was successfully created." }
+        format.json { render :show, status: :created, location: @caminhao_atual }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @caminhao.errors, status: :unprocessable_entity }
+        format.json { render json: @caminhao_atual.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -37,19 +37,19 @@ class CaminhaosController < ApplicationController
   # PATCH/PUT /caminhaos/1 or /caminhaos/1.json
   def update
     respond_to do |format|
-      if @caminhao.update(caminhao_params)
-        format.html { redirect_to caminhao_url(@caminhao), notice: "Caminhao was successfully updated." }
-        format.json { render :show, status: :ok, location: @caminhao }
+      if @caminhao_atual.update(caminhao_params)
+        format.html { redirect_to caminhao_url(@caminhao_atual), notice: "Caminhao was successfully updated." }
+        format.json { render :show, status: :ok, location: @caminhao_atual }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @caminhao.errors, status: :unprocessable_entity }
+        format.json { render json: @caminhao_atual.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /caminhaos/1 or /caminhaos/1.json
   def destroy
-    @caminhao.destroy
+    @caminhao_atual.destroy
 
     respond_to do |format|
       format.html { redirect_to caminhaos_url, notice: "Caminhao was successfully destroyed." }
@@ -60,7 +60,7 @@ class CaminhaosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_caminhao
-      @caminhao = Caminhao.find(params[:id])
+      @caminhao_atual = Caminhao.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
