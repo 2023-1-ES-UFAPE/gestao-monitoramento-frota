dbefd119b35ef421d29ed9bf6ff2b102dbe16726
JuanGustah <juangt360graus@gmail.com>
2024-02-27 02:49:48 -0300

    feat: add navigation to search_motoristas feature

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     get '/search/rota', to: "rota#search", as: :rota_search
     post '/search/rota', to: "rota#search_rota"
 
-    get '/search/motorista', to: "motoristas#search"
+    get '/search/motorista', to: "motoristas#search", as: :motorista_search
     post '/search/motorista', to: "motoristas#search_rotas"
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
