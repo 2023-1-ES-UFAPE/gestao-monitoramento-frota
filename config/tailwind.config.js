c4b6d925cf0587b08302041b7fadf40c4a853a4a
Juan <juangt360graus@gmail.com>
2024-02-27 15:22:33 -0300

    Implementar a navegação completa da aplicação (#61)
    * feat: remade home adding navigation
    
    * feat: add navigation to caminhaos feature
    
    * feat: add navigation to motoristas feature
    
    * feat: add navigation to cargas feature
    
    * feat: add navigation to endereco feature
    
    * feat: add navigation to paradas feature
    
    * feat: add navigation to rotas feature
    
    * feat: add navigation to search_rotas feature
    
    * feat: add navigation to search_motoristas feature
    
    * fix: prepare tailwindcss in build to tests
    
    * fix: fix codacy issues
    
    * fix: add input and select default styles
    
    * fix: fix codacy issues

---
+++ config/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var"],
+      },
+    },
+  },
+};
