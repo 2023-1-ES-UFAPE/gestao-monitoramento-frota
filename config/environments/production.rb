f56b6affb3f95a7d0491d2cc3b8cce37d4482acb
Sara Abreu <50270954+ynjisng@users.noreply.github.com>
2024-04-21 12:20:52 -0300

    devise for production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # configuração pedida pelo devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
